9be0922df29b9ef18a19dbd42fef5eff8ba2d890
Rafael <32114834+Rafael13es@users.noreply.github.com>
2019-05-31 14:03:17 -0500

    Add a new way to get to the max path
    Using this method you first get the size of the pyramid and then go to the center.

--- test1.js
+++ test1.js
@@ -5,7 +5,7 @@
     [8, 5, 9, 3]
 ];*/
 
-piramide = [
+var piramide = [
     [75],
     [95, 64],
     [17, 47, 82],
@@ -23,15 +23,30 @@ piramide = [
     [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23],
 ]
 
-piramide[1][0] = piramide[0][0] + piramide[1][0];
-piramide[1][1] = piramide[0][0] + piramide[1][1];
+console.log(piramide);
+var max = 0;
+
+piramide[1][0] += piramide[0][0];
+piramide[1][1] += piramide[0][0];
 
 for (let i = 2; i < piramide.length; i++) {
-    piramide[i][0] = piramide[i][i - 1] + piramide[i - 1][0];
-    piramide[i][i - 1] = piramide[i][i] + piramide[i - 1][i - 1];
-    for (let j = 1; j <= piramide.length + 1; j++) {
-
+    piramide[i][0] += piramide[i - 1][0];
+    piramide[i][i] += piramide[i - 1][i - 1];
+    for (let j = 1; j < i; j++) {
+        piramide [i][j] = (piramide[i][j] + piramide[i - 1][j - 1]) > (piramide[i][j] + piramide[i - 1][j]) ? 
+        piramide[i][j] + piramide[i - 1][j - 1] : piramide[i][j] + piramide[i - 1][j];
     }
- }
-console.log(piramide);
-console.log("La suma total de la piramide es: " + sum[x - 1]);
+}
+
+max = piramide[piramide.length - 1][0];
+
+for (let i = 0; i < piramide.length - 1; i++) {
+    let aux = piramide[piramide.length - 1][i + 1];
+    if(aux > max){
+        max = aux;
+    }else{
+        max = max;
+    }
+}
+console.log("El valor del camino mas grande que se puede recorrer en la piramide es " + max);
+
