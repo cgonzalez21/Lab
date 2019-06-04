57c18244a938a80023b75597fd0439d9ffc8b129
Rafael13es <rdehermoso@hotmail.com>
2019-06-03 19:15:06 -0500

    Add npm project

---
+++ ejemplo16.js/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const PORT = 3000;
+ 
+app.get('/', function (req, res) {
+    const profesor = {
+        name: "Erick Agrazal",
+        age: "28",
+    };
+    res.json(profesor);
+})
+ 
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}...`)
+});
+
+//Documentacion https://expressjs.com/es/guide/routing.html
+
+//Instalar postman
+
+//Nodemon https://www.npmjs.com/package/nodemon
