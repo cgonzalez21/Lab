d831233d22196198a52aa837f53620249bbf4f69
Rafael13es <rdehermoso@hotmail.com>
2019-06-08 23:59:56 -0500

    install with package.json

--- ejemplo16.js/index.js
+++ ejemplo16.js/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const PORT = 3000;
- 
+
 app.get('/', function (req, res) {
     const profesor = {
         name: "Erick Agrazal",
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
     };
     res.json(profesor);
 })
- 
+
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}...`)
 });
