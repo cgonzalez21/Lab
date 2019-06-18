58811db3b033e571c0e33daaa639413b4c1bd4ff
Rafael13es <rdehermoso@hotmail.com>
2019-06-17 19:07:10 -0500

    fuck this shit

---
+++ ejemplo061719.js
@@ -0,0 +1,12 @@
+
+function getSquare(n){
+    return n** 2;
+}
+
+function getSquareRoot(n){
+    return
+}
+
+module.exports = {
+    getSquare
+}
--- ejemplo17.js/index.js
+++ ejemplo17.js/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 const path = require('path');
 
+const { getSquare } = require('../ejemplo061719.js');
+
 //Configuraciones
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser());
 
 
 //Archivos estaticos
@@ -14,6 +18,11 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('form.pug')
+});
+
+app.post('/', (req, res) =>{
+    const { number } = req.body;
+    res.json({message: `El numero es ${getSquare(number)}`});
 });
 
 app.listen(port, () => {
